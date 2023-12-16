f1d2c725dc4b0b344357e6f9cabbe10c983a0ce0
Awsomenesss <>
2023-12-16 22:51:02 +0000

    created a signup form that creates new acount and communicates errors to users

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://bjj-fcb7bcc1efc9.herokuapp.com";
+axios.defaults.baseURL = 'https://pp5-bjj-api-2269f4220822.herokuapp.com/';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
